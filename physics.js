4ab3022da5906a1bf3d0c112932fb686a431d50e
Daniel Todd <danielgarrytodd@gmail.com>
2025-03-13 18:08:18 +0000

    beans! now with motion

--- physics.js
+++ physics.js
@@ -301,6 +301,7 @@ function drawStats() {
     ctx.fillText(`Force Deviation: ${forceDeviation.toFixed(2)}`, 10, 60);
     ctx.fillText(`Accel X: ${accelX.toFixed(2)}`, 10, 80);
     ctx.fillText(`Accel Y: ${accelY.toFixed(2)}`, 10, 100);
+    ctx.fillText(`Accel Y: ${lastShakeTime}`, 10, 120);
 }
 
 function detectMouseCollisions() {
